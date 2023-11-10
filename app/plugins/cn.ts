d39a26ba98a7d16a5dc841abe53a8f80990f5bbf
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-11-10 17:53:03 +0800

    Update cn.ts

--- app/plugins/cn.ts
+++ app/plugins/cn.ts
@@ -2,7 +2,7 @@ import { BuiltinPlugin } from "./typing";
 
 export const CN_PLUGINS: BuiltinPlugin[] = [
   {
-    name: "搜索引擎",
+    name: "联网功能",
     toolName: "web-search",
     lang: "cn",
     description: "搜索引擎的网络搜索功能工具。",
@@ -20,7 +20,7 @@ export const CN_PLUGINS: BuiltinPlugin[] = [
     enable: true,
   },
   {
-    name: "网页浏览器",
+    name: "网页爬虫",
     toolName: "web-browser",
     lang: "cn",
     description:
@@ -43,24 +43,14 @@ export const CN_PLUGINS: BuiltinPlugin[] = [
     toolName: "dalle_image_generator",
     lang: "cn",
     description:
-      "DALL·E 可以根据自然语言的描述创建逼真的图像和艺术。使用本插件需要配置 Cloudflare R2 对象存储服务。",
+      "DALL·E 可以根据自然语言的描述创建逼真的图像和艺术。",
     builtin: true,
     createdAt: 1694703673000,
     enable: false,
   },
   {
-    name: "Stable Diffusion",
-    toolName: "stable_diffusion_image_generator",
-    lang: "cn",
-    description:
-      "Stable Diffusion 图像生成模型。使用本插件需要配置 Cloudflare R2 对象存储服务以及 stable-diffusion-webui 接口。",
-    builtin: true,
-    createdAt: 1688899480510,
-    enable: false,
-  },
-  {
-    name: "Arxiv",
-    toolName: "arxiv",
+    name: "ArXiv论文库",
+    toolName: "ArXiv论文库",
     lang: "cn",
     description: "使用 Arxiv 接口搜索并获取文章信息。",
     builtin: true,
