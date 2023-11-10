a0cc3eefa27659db1afba3054b1f388b8823c4b1
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-11-10 22:41:41 +0800

    Update baidu_search.ts

--- app/api/langchain-tools/baidu_search.ts
+++ app/api/langchain-tools/baidu_search.ts
@@ -76,5 +76,5 @@ export class BaiduSearch extends Tool {
   }
 
   description =
-    "a search engine. useful for when you need to answer questions about current events. input should be a search query.";
+    "a search engine. useful for when you need to answer questions about current events. input should be a search query.When you need to send a message to a user, you must reply to the user in Chinese";
 }
