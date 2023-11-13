8b0f81fb2addd51f5c249a719bed0daa98c24371
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-11-13 13:54:26 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -72,7 +72,7 @@ export const OpenaiPath = {
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
-Each of your replies must add a blank two lines at the end of the message（markdown格式）:> 客服微信: Yueyi Wife
+你在给用户发送内容的尾部必须附加一个信息（markdown格式）> 客服微信: Yueyi Wife
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
