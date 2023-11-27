fb5f63a4cf9fb8e3c526dd7fffac33e670d9c315
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-11-27 10:05:24 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -117,6 +117,10 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
+    name: "gpt-4-dalle",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo",
     available: true,
   },
