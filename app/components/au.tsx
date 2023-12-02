980dc0e2bbabef0877691e8b5737872f192779e8
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-12-02 16:45:19 +0800

    Update au.tsx

--- app/components/au.tsx
+++ app/components/au.tsx
@@ -6,19 +6,22 @@ export function AuPage() {
 
   useEffect(() => {
     function loadWebpage() {
-      const iframe = document.createElement("iframe");
-      iframe.src = "https://zfb.yjie.fun/zl/";
-      iframe.style.width = "100%";
-      iframe.style.height = "100%";
-      iframe.style.border = "none";
-      webpageRef.current.appendChild(iframe);
+      if (webpageRef.current !== null) {
+        const iframe = document.createElement("iframe");
+        iframe.src = "https://zfb.yjie.fun/zl/";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        webpageRef.current.appendChild(iframe);
 
-      // 隐藏侧边滚动条
-      document.body.style.overflow = "hidden";
+        // 隐藏侧边滚动条
+        document.body.style.overflow = "hidden";
+      }
     }
 
     loadWebpage();
   }, []);
+
   return (
     <div className={styles["auth-page"]} style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "white" }}>
       <div ref={webpageRef} style={{ width: "100%", height: "100%", overflow: "hidden" }}></div>
