773c74d153fd60dd2cbaac31abcb34937b7c0d68
ssrsss <129376251+ssrsss@users.noreply.github.com>
2023-12-18 01:31:33 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -159,7 +159,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          天工智能AI试用版
+          天工智能AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
           欢迎您,我是您的多功能AI问答助理~
